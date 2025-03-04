be41b651667d6e6ddaf294fbd4c327200a33955f
dewandrare <dewandraaye@gmail.com>
2025-03-04 15:08:46 +0700

    test on js logo

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+/*
+if ('WebSocket' in window) {
+    var socket = new WebSocket(address);
+    socket.onmessage = function (msg) {
+        console.log('WebSocket message received:', msg.data);
+        if (msg.data == 'reload') window.location.reload();
+        else if (msg.data == 'refreshcss') refreshCSS();
+    };
+}
+    */
